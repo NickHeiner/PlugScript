158bbe60df5782ca4673f97dfc603c59fffa16f7
Nick Heiner <nick.heiner@opower.com>
2014-08-27 17:23:27 -0400

    fixing a test

--- test/unit/tokenize.test.js
+++ test/unit/tokenize.test.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect,
     tokenize = require('../../lib/tokenize'),
     destreamify = require('../lib/destreamify');
 
-describe('tokenize', function() {
+describe.only('tokenize', function() {
 
     var runTokenize;
 
@@ -14,23 +14,23 @@ describe('tokenize', function() {
     });
 
     it('splits words', function() {
-        return expect(runTokenize('foo bar baz')).to.eventually.equal(['foo', 'bar', 'baz']);
+        return expect(runTokenize('foo bar baz')).to.eventually.deep.equal(['foo', 'bar', 'baz']);
     });
 
     it('splits parens', function() {
-        return expect(runTokenize('(expr)')).to.eventually.equal(['(', 'expr', ')']);
+        return expect(runTokenize('(expr)')).to.eventually.deep.equal(['(', 'expr', ')']);
     });
 
     it('splits operators', function() {
-        return expect(runTokenize('fun foo n -> n')).to.eventually.equal(['fun', 'foo', 'n', '->', 'n']);
+        return expect(runTokenize('fun foo n -> n')).to.eventually.deep.equal(['fun', 'foo', 'n', '->', 'n']);
     });
 
     it('does not include newlines', function() {
-       return expect(runTokenize('fib\n3')).to.eventually.equal(['fib', '3']); 
+       return expect(runTokenize('fib\n3')).to.eventually.deep.equal(['fib', '3']); 
     });
 
     it('strips whitespace', function() {
-       return expect(runTokenize('1 + 2')).to.eventually.equal(['1', '+', '2']); 
+       return expect(runTokenize('1 + 2')).to.eventually.deep.equal(['1', '+', '2']); 
     });
 
 });
